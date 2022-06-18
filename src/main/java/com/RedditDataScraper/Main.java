5f36215f91dd04a75723ac806152f39c09cdb1e2
amoghmc <63165759+amoghmc@users.noreply.github.com>
2022-06-18 23:04:41 +0530

    Throw FileException for incorrect db path

--- src/main/java/com/RedditDataScraper/Main.java
+++ src/main/java/com/RedditDataScraper/Main.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		String filename = "databaseLocation.txt";
+		String filename = "Database/databaseLocation.txt";
 		try {
 			new JFrameHandler(filename);
 		}
