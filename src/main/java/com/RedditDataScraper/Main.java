7c608e471b6577baf4d9ef1c91cc2822e2174303
amoghmc <63165759+amoghmc@users.noreply.github.com>
2022-07-20 04:55:08 +0530

    Remove unwanted file exception

--- src/main/java/com/RedditDataScraper/Main.java
+++ src/main/java/com/RedditDataScraper/Main.java
@@ -6,14 +6,10 @@ import java.sql.*;
 public class Main {
 	private static final String db_path = "jdbc:sqlite::resource:data_scraper";
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		try {
 			new JFrameHandler(db_path);
-		}
-		catch (FileNotFoundException e) {
-			throw e;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 
